ba4f629ada3b85eeee9838c5d1509dbc7a1d8056
Phạm Trường <phamtruong04112004@gmail.com>
2026-08-29 12:54:28 +0700

    resolve confilct

--- back-end/src/main/java/com/pht/dev_edu/quiz/repo/QuizRepo.java
+++ back-end/src/main/java/com/pht/dev_edu/quiz/repo/QuizRepo.java
@@ -16,20 +16,24 @@ public interface QuizRepo extends JpaRepository<QuizEntity, UUID> {
                         WHERE   course_id           = :courseId
                         AND     status              = :status
                         AND     (created_at, id)    <= (:lastTimestamp, :lastId)
+                        AND     (:keyword IS NULL OR :keyword = '' OR immutable_unaccent(title) ILIKE immutable_unaccent(CONCAT('%', :keyword, '%')))
+                        AND     deleted_at          IS NULL
                         ORDER BY created_at DESC, id DESC
                         LIMIT :limit
                         """, nativeQuery = true)
-        List<QuizEntity> findByCourseIdAndDeletedAtIsNull(UUID courseId, String status, UUID lastId,
+        List<QuizEntity> findByCourseIdAndDeletedAtIsNull(UUID courseId, String status, String keyword, UUID lastId,
                         LocalDateTime lastTimestamp, int limit);
 
         @Query(value = """
                         SELECT *
                         FROM quizzes
                         WHERE   status              = :status
+                        AND     (:keyword IS NULL OR :keyword = '' OR immutable_unaccent(title) ILIKE immutable_unaccent(CONCAT('%', :keyword, '%')))
+                        AND     deleted_at          IS NULL
                         AND     (created_at, id)    <= (:lastTimestamp, :lastId)
                         ORDER BY created_at DESC, id DESC
                         LIMIT :limit
                         """, nativeQuery = true)
-        List<QuizEntity> findByStatusAndDeletedAtIsNull(String status, UUID lastId, LocalDateTime lastTimestamp,
+        List<QuizEntity> findByStatusAndDeletedAtIsNull(String status, String keyword, UUID lastId, LocalDateTime lastTimestamp,
                         int limit);
 }
